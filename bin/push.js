4bf7c9d413cd006a25d03a85ca0cb0e1311764d9
a13821190779 <945039036@qq.com>
2018-02-09 23:28:20 +0800

    'Update:测试自动化commit'

---
+++ bin/push.js
@@ -0,0 +1,10 @@
+const shell = require('shelljs');
+const yargs = require('yargs').argv
+if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git');
+    shell.exit(1);
+}
+shell.exec('git status');
+shell.exec('git add .');
+shell.exec(`git commit -m '${yargs.m}'`);
+shell.exec('git push');
